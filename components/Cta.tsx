d7fde60d93dfee895669c7090aa68f00f89418fd
[mahtot] <[mahtotgher13@gmail.com]>
2024-09-09 12:53:48 +0300

    Some syntax error fixes

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -7,7 +7,7 @@ const Cta = () => {
                 Got Amazing Hair Tips or Stories?
             </h1>
             <p className="text-lg text-gray-700 mb-8 max-w-xl mx-auto leading-relaxed font-montserrat">
-                Whether you're a professional hairstylist or simply passionate about hair care,
+                Whether you are a professional hairstylist or simply passionate about hair care,
                 your knowledge and stories are invaluable. The world is ready for your tips and experiences!
             </p>
             <Link href={`blogs`} className="mt-auto max-w-max p-2 px-10 rounded-sm flex gap-2 border border-[#b57d6a] bg-white text-[#9C3A14] hover:text-white hover:bg-[#9C3A14] transition-all duration-300">
